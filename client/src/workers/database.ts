df878a5080538b8398b271d89439c104b49f0681
tett23 <tett23@gmail.com>
2024-07-15 18:23:15 +0900

    Move DB codes to shared worker

--- client/src/workers/database.ts
+++ client/src/workers/database.ts
@@ -1,21 +1,65 @@
-import sqlite3InitModule, {
-  sqlite3Worker1Promiser,
-} from "@sqlite.org/sqlite-wasm";
+import sqlite3InitModule, { type Database } from "@sqlite.org/sqlite-wasm";
 
-async function* databaseGenerator() {
-  const sqlite3 = sqlite3InitModule();
-  const db = sqlite3.oo1.DB("/database");
+async function* databaseGenerator(): AsyncGenerator<
+  import("file:///Users/tett23/Library/Caches/deno/npm/registry.npmjs.org/@sqlite.org/sqlite-wasm/3.46.0-build2/index").Database,
+  void,
+  unknown
+> {
+  const sqlite3 = await sqlite3InitModule();
+  const arrayBuffer = await (await fetch("/database")).arrayBuffer();
+  const db = new sqlite3.oo1.DB();
+  const deserializeFlags = sqlite3.capi.SQLITE_DESERIALIZE_FREEONCLOSE |
+    sqlite3.capi.SQLITE_DESERIALIZE_RESIZEABLE;
+  const p = sqlite3.wasm.allocFromTypedArray(arrayBuffer);
+  const rc = sqlite3.capi.sqlite3_deserialize(
+    db.pointer as number,
+    "main",
+    p,
+    arrayBuffer.byteLength,
+    arrayBuffer.byteLength,
+    deserializeFlags,
+  );
+  db.checkRc(rc);
+
+  console.log(db);
+  db.exec({
+    sql: `
+      INSERT INTO Page (
+        id,
+        slug,
+        title,
+        content,
+        updatedAt
+      )
+      VALUES (
+        $id,
+        $slug,
+        $title,
+        $content,
+        $updatedAt
+      )
+  `,
+    bind: {
+      "$id": "home",
+      "$slug": "home",
+      "$title": "Home",
+      "$content": "Welcome to the home page.",
+      "$updatedAt": new Date().toISOString(),
+    },
+  });
+  console.log(db.exec({ sql: "select * from Page;", rowMode: "array" }));
+
   while (true) {
     yield db;
   }
 }
 
 const databaseInstance = databaseGenerator();
-async function db() {
+async function database(): Promise<Database> {
   return (await databaseInstance.next()).value;
 }
 
-self.addEventListener("connect", (event) => {
+self.addEventListener("connect", async (event) => {
   console.log("Database worker connected");
   const port = (() => {
     if (("ports" in event && Array.isArray(event.ports))) {
@@ -24,13 +68,17 @@ self.addEventListener("connect", (event) => {
       throw new Error("No ports found");
     }
   })();
+  const db = await database();
 
   port.onmessage = async (e) => {
     const { type, payload } = e.data;
 
     switch (type) {
       case "get":
-        port.postMessage({ type, db: await db() });
+        port.postMessage({
+          type,
+          payload: db.exec({ sql: "select * from Page;", rowMode: "array" }),
+        });
         break;
       case "set":
         port.postMessage({ type });
